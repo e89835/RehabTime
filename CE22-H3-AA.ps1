2138a33365b3fc2aeaf775476cdde1ba4ca9075c
e89835 <haryde91@hotmail.com>
2022-08-20 19:15:44 +0200

    Close #31 CE 22 H3 Update Automatic Updates
    Updated script for Automatic Updates in H3

---
+++ CE22-H3-AA.ps1
@@ -0,0 +1,12 @@
+﻿# CE 22 H3 Automation script
+# Please note to enter the login user & pwd as arg1 & arg2
+# The Docker daemon must be running
+
+$user=$args[0]
+$pwd=$args[1]
+
+cd C:\Users\JuanJo\Desktop\MII\2122\CC\Proyecto\rehabtime
+docker login -u $user
+$pwd
+docker tag node-alpine:latest e89835/rehabtime:rehabtime-node # Tag source-image target-image 
+docker push e89835/rehabtime:rehabtime-node # Push name-tag
